3534242932f8dec5fa8124cf1a517817e2fb9fae
Joice Figueiredo <87450298+JoyFigueiredo@users.noreply.github.com>
2025-10-30 09:43:13 -0300

    Update app.routes.ts
    Update app.routes.ts

--- app-alimentador-auto/src/app/app.routes.ts
+++ app-alimentador-auto/src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-  {path:'', redirectTo: 'login', pathMatch:'full'},
-  { path: "login", component: LoginComponent },
-  {}
+  {path: '', redirectTo: 'login', pathMatch:'full'},
+  {path: "login", component: LoginComponent },
+  {path: "alimentar", component: AlimentarComponent },
+  {path: "historico", component: HistoricoComponent }
 ];
+
